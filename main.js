621ad3be0869001b4123439f22991915f4871fca
aniareyouok <84382283+aniareyouok@users.noreply.github.com>
2021-11-22 21:28:55 +0100

    created check name and password function

---
+++ main.js
@@ -0,0 +1,23 @@
+const usernameInput = document.getElementById('username')
+usernameInput.addEventListener('keyup', checkName);
+const usernameError = document.getElementById('username_error');
+
+function checkName(e) {
+    if(e.target.value.includes('@')) {
+        usernameError.textContent = "Het gebruikersnaam mag geen '@' bevatten!"
+    } else {
+        usernameError.textContent = '';
+    }
+}
+
+const passwordInput = document.getElementById('password');
+passwordInput.addEventListener('keyup', checkPassword);
+const passwordError = document.getElementById('password_error');
+
+function checkPassword(e) {
+    if(e.target.value !== '' && e.target.value.length <= 6) {
+        passwordError.textContent = "Het wachtwoord moet minimaal 6 tekens bevatten!"
+    } else {
+        passwordError.textContent = '';
+    }
+}
